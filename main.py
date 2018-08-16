16f2d3b0e24717daf0006a528cf8ffe21b3bab2a
atilarangel <superatilarangel@gmail.com>
2018-08-16 18:52:55 -0300

    fix: main.py migrate from mac to windows & up: result

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import subprocess
 import json, ast
 import time
+import os
 
 startTime = time.time()
 allcontent = {}
 resulFinal = open("resultado.txt", "w")
 count = 3005
-# url = 'http://doweb.rio.rj.gov.br/ler_pdf.php?page=0&edi_id=%s'%(count)
 
+index = str("'./index'")
 while (count <= 3827):
-    subprocess.call("node -e 'require(`./index`).allTrends(`http://doweb.rio.rj.gov.br/ler_pdf.php?page=0&edi_id=%s`)'"%(count), shell=True)
-    print "Done %s"%(count)
-    json_data = json.load(open('allwords.json'))
+    url = str("'http://doweb.rio.rj.gov.br/ler_pdf.php?page=0&edi_id=%s'"%(count))
+    subprocess.call(u'node -e "require(%s).allTrends(%s)"'%(index, url), shell=True)
+    print u"Done %s"%(count)
+    json_data = json.load(open(u'allwords.json'))
     json_data = ast.literal_eval(json.dumps(json_data))
     # print json_data
     for item in json_data:
@@ -21,10 +23,10 @@ while (count <= 3827):
             allcontent[item] = 1
     count+=1
 result = sorted(allcontent.items(), key=lambda t: t[1], reverse=True)
-resulFinal.write("\nResultado Final: \n")
+resulFinal.write(u"\nResultado Final: \n")
 for item in result:
     if item[1] != 1:
-        resulFinal.write("%s - %s\n"%(item[1], item[0]))
+        resulFinal.write(u"%s - %s\n"%(item[1], item[0]))
 resulFinal.close()
 endTime = time.time()
 print(endTime - startTime)
